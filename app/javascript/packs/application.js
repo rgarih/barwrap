bb1e991e0803b391a7aaf0ae4e99b35276862a6d
garih rachel <rgarih@gmail.com>
2019-09-18 12:29:29 +0300

    sign_up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,14 @@
 import "bootstrap";
 import Typed from 'typed.js';
-// const homeText = () => {
-//   new Typed('#landing-text', {
-//     strings: ["We’ve put together a way for you to get a live feed on bars and clubs near you!" ,"No more wondering where to go for a good time! We tailor bars based on YOU!"],
-//     typeSpeed: 50,
-//     loop: true
-//   });
-// }
+const homeText = () => {
+  new Typed('#landing-text', {
+    strings: ["We’ve put together a way for you to get a live feed on bars and clubs near you!" ,"No more wondering where to go for a good time! We tailor bars based on YOU!"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
 
-// homeText();
+ homeText();
 
 const multiSelectRecommendedPage = () => {
 const labels = document.querySelectorAll('.label-check');
