93186e2c7172e31a2303cf04b4bac1d5f89b6d14
Brenda Sanchez <brendals212@gmail.com>
2019-09-19 10:10:30 +0300

    map changes

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,6 +5,8 @@ mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
 return new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v10',
+    center: [32.062238, 34.774845],
+    zoom: 3
   });
 };
 const addMarkersToMap = (map, markers) => {
@@ -16,10 +18,11 @@ new mapboxgl.Marker()
       .addTo(map);
   });
 };
+
 const fitMapToMarkers = (map, markers) => {
 const bounds = new mapboxgl.LngLatBounds();
 markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-map.fitBounds(bounds, { padding: 90, maxZoom: 70, duration: 5000 });
+// map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 10 });
 };
 const initMapbox = () => {
 if (mapElement) {
