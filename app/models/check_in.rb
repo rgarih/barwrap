cac5c3ac17eadc9a20372f445c3222f5ea354a8f
Gilad Sarusi <giladsar@gmail.com>
2019-09-21 00:19:15 +0300

    styling debugging

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -2,8 +2,10 @@ class CheckIn < ApplicationRecord
   belongs_to :user
   belongs_to :place
 
-  TYPE_OF_MUSIC = ["Rock", "Hip Hop", "Techno", "Mainstream", "Reggae", "Reggaeton", "Other"]
-  validates :type_of_music, inclusion: { in: TYPE_OF_MUSIC }, presence: false
+  TYPE_OF_MUSIC = ["Rock", "Hip Hop", "Techno", "Mainstream", "Reggae", "Reggaeton", "Hard to tell🤔"]
+  validates :type_of_music, inclusion: { in: TYPE_OF_MUSIC }
+  validates :type_of_music,  presence: false
+
   mount_uploader :photo, PhotoUploader
 
 end
