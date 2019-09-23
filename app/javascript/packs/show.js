8590dca121bb24cb22abb619ba9a4994a739b46e
Gilad Sarusi <giladsar@gmail.com>
2019-09-23 11:11:10 +0300

    checkin are cliakable however double tap at first

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,13 +1,20 @@
 const dropDownCheckIn = () => {
-const checkins = document.querySelectorAll('.dropdown-checkin');
+  const checkins = document.querySelectorAll('.dropdown-checkin');
   checkins.forEach(checkin => {
     checkin.addEventListener('click', (event) => {
-      const snapshot = event.target.querySelector('#snapshot');
-        if (snapshot.style.display === "none") {
-          snapshot.style.display = "block";
-        } else {
-          snapshot.style.display = "none";
-        }
+      // console.log(event.target);
+      const icon = checkin.querySelector('.fas');
+      const snapshot = checkin.querySelector('.snapshot');
+      console.log(checkin);
+      console.log(icon);
+      console.log(snapshot);
+      if (snapshot.style.display === "none") {
+        snapshot.style.display = "block";
+        icon.style.display = 'none';
+      } else {
+        snapshot.style.display = "none";
+        icon.style.display = 'block';
+      }
     })
   })
 }
